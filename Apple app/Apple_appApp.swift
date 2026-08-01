1ffbe903fbb3dd818c5926a2aa536af6f1eda9fa
nayar <nayaralsoulmi@gmail.com>
2026-08-01 18:14:18 +0300

    Initial Commit

---
+++ Apple app/Apple_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_appApp.swift
+//  Apple app
+//
+//  Created by Nayar Alsoulmi on 18/02/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct Apple_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple app
+//
+//  Created by Nayar Alsoulmi on 18/02/1448 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
